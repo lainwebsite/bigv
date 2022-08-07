89366205be9013d9d6bda6477fd855a138ede13d
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-08-07 17:08:28 +0700

    feat: controller setup

---
+++ app/Http/Controllers/User/TransactionDiscountController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\TransactionDiscount;
+use Illuminate\Http\Request;
+
+class TransactionDiscountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TransactionDiscount  $transactionDiscount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TransactionDiscount $transactionDiscount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TransactionDiscount  $transactionDiscount
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TransactionDiscount $transactionDiscount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TransactionDiscount  $transactionDiscount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TransactionDiscount $transactionDiscount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TransactionDiscount  $transactionDiscount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TransactionDiscount $transactionDiscount)
+    {
+        //
+    }
+}
