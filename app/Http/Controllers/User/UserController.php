51cb306b4c1a4e93338425303ece952c245a6e3c
hendra047 <hdtaki04@gmail.com>
2022-08-10 18:58:27 +0700

    User - Profile Detail

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -82,5 +83,12 @@ class UserController extends Controller
     public function destroy(User $user)
     {
         //
+    }
+
+    public function profile()
+    {
+        if (Auth::check()) {
+            return view('user.account');
+        }
     }
 }
