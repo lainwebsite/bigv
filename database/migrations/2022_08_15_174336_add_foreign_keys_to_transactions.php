0118f505000a5a6fe1f975619accb0f459835302
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-08-21 20:35:08 +0700

    feat: add user_id on transactions

--- database/migrations/2022_08_15_174336_add_foreign_keys_to_transactions.php
+++ database/migrations/2022_08_15_174336_add_foreign_keys_to_transactions.php
@@ -22,6 +22,8 @@ return new class extends Migration
             $table->foreign('pickup_time_id')->references('id')->on('pickup_times')->onDelete('cascade');
             $table->unsignedBigInteger('status_id')->index();
             $table->foreign('status_id')->references('id')->on('transaction_statuses')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
