79fa832aca89ffd0ec5d959d75bd18620c05ce01
hendra047 <hdtaki04@gmail.com>
2022-08-27 11:17:08 +0700

    User - Transaction History (not done)

---
+++ resources/views/user/transactionHistory.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Transaction History</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://unpkg.com/feather-icons"></script>
+</head>
+<body>
+    <div class="mt-4 mb-3">
+        <h1>Transaction History</h1>
+    </div>
+    <div class="container my-3">
+        <div class="border border-secondary">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Street</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($transactions as $transaction)
+                        <tr>
+                            <th scope="row">{{ $loop->index + 1 }}</th>
+                            <td>{{ $address->name }}</td>
+                            <td>{{ $address->street }}</td>
+                            <td>{{ $address->phone }}</td>
+                            <td>
+                                <a href="{{ route('user.user-address.show', $address->id) }}" class="btn btn-info">Detail</a>
+                                <a href="{{ route('user.user-address.edit', $address->id) }}" class="btn btn-warning">Edit</a>
+                                <form method="POST" action="{{ route('user.user-address.destroy', $address->id) }}">
+                                    @method('DELETE')
+                                    @csrf
+                                    
+                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this address?')">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script>
+        feather.replace();
+    </script>
+</body>
+</html>
