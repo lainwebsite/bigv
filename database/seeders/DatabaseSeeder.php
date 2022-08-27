28d5ae290ff31e1097d36b5ae93fd8cb0312e9fd
hendra047 <hdtaki04@gmail.com>
2022-08-27 11:25:56 +0700

    feat: additional merge branch 'base' into user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,45 +8,42 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+        /**
+         * Seed the application's database.
+         *
+         * @return void
+         */
+        public function run()
+        {
+                // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+                // \App\Models\User::factory()->create([
+                //     'name' => 'Test User',
+                //     'email' => 'test@example.com',
+                // ]);
 
-<<<<<<< HEAD
-        // $this->call(DiscountTypeSeeder::class);
-        // $this->call(PaymentMethodSeeder::class);
-        // $this->call(PickupMethodSeeder::class);
+                // $this->call(DiscountTypeSeeder::class);
+                // $this->call(PaymentMethodSeeder::class);
+                // $this->call(PickupMethodSeeder::class);
 
-        // $this->call(VendorSeeder::class);
-        // $this->call(ProductCategorySeeder::class);
-        // $this->call(ProductSeeder::class);
-        // $this->call(ProductVariationSeeder::class);
-        $this->call(ProductImageSeeder::class);
+                // $this->call(VendorSeeder::class);
+                // $this->call(ProductCategorySeeder::class);
+                // $this->call(ProductSeeder::class);
+                // $this->call(ProductVariationSeeder::class);
+                $this->call(ProductImageSeeder::class);
 
-        // $this->call(TransactionStatusSeeder::class);
-        // $this->call(UserRoleSeeder::class);
-        // $this->call(UserTierSeeder::class);
-        // $this->call(UserSeeder::class);
-        // $this->call(UserAddressSeeder::class);
-=======
-        $this->call(DiscountTypeSeeder::class);
-        $this->call(PaymentMethodSeeder::class);
-        $this->call(PickupMethodSeeder::class);
-        $this->call(TransactionStatusSeeder::class);
-        $this->call(UserRoleSeeder::class);
-        $this->call(UserTierSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(PickupTimeSeeder::class);
->>>>>>> origin/base
-    }
+                // $this->call(TransactionStatusSeeder::class);
+                // $this->call(UserRoleSeeder::class);
+                // $this->call(UserTierSeeder::class);
+                // $this->call(UserSeeder::class);
+                // $this->call(UserAddressSeeder::class);
+                $this->call(DiscountTypeSeeder::class);
+                $this->call(PaymentMethodSeeder::class);
+                $this->call(PickupMethodSeeder::class);
+                $this->call(TransactionStatusSeeder::class);
+                $this->call(UserRoleSeeder::class);
+                $this->call(UserTierSeeder::class);
+                $this->call(UserSeeder::class);
+                $this->call(PickupTimeSeeder::class);
+        }
 }
